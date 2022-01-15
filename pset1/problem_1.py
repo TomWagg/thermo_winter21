6c7b91179dc6326814ee9f69e15f994ecd84e630
Tom Wagg <twagg@college.harvard.edu>
2022-01-15 09:27:59 -0800

    try running a longer one

--- pset1/problem_1.py
+++ pset1/problem_1.py
@@ -5,15 +5,16 @@ import numpy as np
 from time import time
 
 # create a new class for the simulation with some randomish variable choices
-sim = Simulation(N=100, E=100, size=750, radius=3, mass=1, delay=0, visualise=False)
+sim = Simulation(N=100, E=10, size=750, radius=3, mass=1, delay=0, visualise=False)
 
-vels = [np.sqrt(sum(p.vel**2)) for p in sim.particles]
-# print("INITIAL", vels)
+vels = np.array([np.sqrt(sum(p.vel**2)) for p in sim.particles])
+
+print("initial v_rms", np.sqrt(np.mean(vels**2)))
+
 plt.figure()
 
 
-INITIAL_STEPS = 2500
-
+INITIAL_STEPS = 5000
 
 # run the simulation
 start = time()
@@ -29,6 +30,8 @@ print("Runtime: {:1.2f}s".format(time() - start))
 v_range = np.linspace(0, np.ceil(vels.max()), 1000)
 v_rms = np.sqrt(np.mean(vels**2))
 
+print("final v_rms", np.sqrt(np.mean(vels**2)))
+
 kBT = v_rms**2 / 3
 
 plt.plot(v_range, v_range / kBT * np.exp(-v_range**2 / (2 * kBT)), label="Analytic distribution")
@@ -40,5 +43,4 @@ plt.ylabel(r"$\mathrm{d}N/\mathrm{d}v$")
 
 plt.legend()
 
-# print("FINAL", vels)
 plt.show()
