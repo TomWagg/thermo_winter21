853c06d81d83b8d7384ae71e145d957ae7cf3bee
Tom Wagg <twagg@college.harvard.edu>
2022-01-15 22:35:12 -0800

    something wrong on c, masses colliding weirdly

--- pset1/problem_2.py
+++ pset1/problem_2.py
@@ -55,9 +55,56 @@ def part_b(steps=10000):
 
     plt.show()
 
+def part_c(steps=10000):
+
+    # create a new class for the simulation with some randomish variable choices
+    N = 300
+    sim = Simulation(N=N, E=0.1, size=750, radius=3,
+                     masses=np.concatenate([np.repeat(1, N // 2), np.repeat(100, N - N // 2)]),
+                     delay=1, visualise=True)
+
+    # start a timer and create an empty velocity array
+    start = time()
+    velocities = []
+
+    # run the simulation until the given number of steps and save the new velocities
+    sim.run_simulation(steps=steps)
+    velocities.extend(np.sqrt(np.sum(sim.vel**2, axis=1)))
+
+    # keep running the simulation 100 more times but for 100x less time each
+    # motivation behind this is to increase the sample size and make a nicer histogram
+    for _ in range(100):
+        sim.run_simulation(steps=steps // 100)
+        velocities.extend(np.sqrt(np.sum(sim.vel**2, axis=1)))
+    velocities = np.array(velocities)
+    print("Runtime: {:1.2f}s".format(time() - start))
+
+    # save the velocities for later (just in case)
+    np.save("vels_2c.npy", velocities)
+
+    # work out the value of k_B T from velocities
+    v_rms = np.sqrt(np.mean(velocities**2))
+    kBT = v_rms**2 / 2
+
+    # start a figure
+    fig, ax = plt.subplots()
+
+    # plot the analytic Maxwellian
+    v_range = np.linspace(0, np.ceil(velocities.max()), 1000)
+    ax.plot(v_range, velocity_dist(v_range, kBT), label="Analytic distribution")
+
+    # plot a histogram of the velocities
+    ax.hist(velocities, bins="fd", density=True, label="Simulated distribution")
+
+    # label axes, add a legend
+    ax.set_xlabel(r"Velocity, $v$")
+    ax.set_ylabel(r"$\mathrm{d}N/\mathrm{d}v$")
+    ax.legend()
+
+    plt.show()
 
 def main():
-    part_b()
+    part_c(10000)
 
 
 if __name__ == "__main__":
