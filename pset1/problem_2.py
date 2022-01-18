5bc941cb1ac4eb4ebd5cdb0d2a1c239bed8f675d
Tom Wagg <twagg@college.harvard.edu>
2022-01-18 15:14:40 -0800

    add energy plot to 2b solution

--- pset1/problem_2.py
+++ pset1/problem_2.py
@@ -5,15 +5,19 @@ import numpy as np
 from time import time
 
 
-def velocity_dist(v, kBT):
-    return v / kBT * np.exp(-v**2 / (2 * kBT))
+def velocity_dist(v, v_rms):
+    return v / (0.5 * v_rms**2) * np.exp(-(v / v_rms)**2)
+
+
+def energy_dist(E, m, v_rms):
+    return E / (0.5 * v_rms**2) * np.exp(-E / (0.5 * m * v_rms**2))
 
 
 def part_b(steps=10000):
     assert steps >= 100
 
     # create a new class for the simulation with some randomish variable choices
-    sim = Simulation(N=300, E=1, size=750, radius=3, masses=1, delay=10, visualise=True)
+    sim = Simulation(N=300, E=1, size=750, radius=3, masses=1, delay=10, visualise=False)
 
     # start a timer and create an empty velocity array
     start = time()
@@ -32,27 +36,38 @@ def part_b(steps=10000):
     print("Runtime: {:1.2f}s".format(time() - start))
 
     # save the velocities for later (just in case)
-    np.save("data/vels_2b.npy", velocities)
+    # np.save("data/vels_2b.npy", velocities)
 
     # work out the value of k_B T from velocities
     v_rms = np.sqrt(np.mean(velocities**2))
-    kBT = v_rms**2 / 2
 
     # start a figure
-    fig, ax = plt.subplots()
+    fig, axes = plt.subplots(1, 2, figsize=(16, 7))
 
-    # plot the analytic Maxwellian
+    # plot the analytic velocity distribution
     v_range = np.linspace(0, np.ceil(velocities.max()), 1000)
-    ax.plot(v_range, velocity_dist(v_range, kBT), label="Analytic distribution", lw=5)
+    axes[0].plot(v_range, velocity_dist(v_range, v_rms), label="Analytic distribution", lw=5)
 
     # plot a histogram of the velocities
-    ax.hist(velocities, bins="fd", density=True, label="Simulated distribution",
-            color="tab:purple", alpha=0.8)
+    axes[0].hist(velocities, bins="fd", density=True, label="Simulated distribution",
+                 color="tab:purple", alpha=0.8)
 
     # label axes, add a legend
-    ax.set_xlabel(r"Velocity, $v$")
-    ax.set_ylabel(r"$\mathrm{d}N/\mathrm{d}v$")
-    ax.legend()
+    axes[0].set_xlabel(r"Velocity, $v \, [\rm cm \, s^{-1}]$")
+    axes[0].set_ylabel(r"$\mathrm{d}N/\mathrm{d}v$")
+    axes[0].legend()
+
+    # repeat for the energies
+    energies = 0.5 * velocities**2
+    E_range = np.linspace(0, np.ceil(energies.max()), 1000)
+    axes[1].plot(E_range, energy_dist(E_range, 1, v_rms), label="Analytic distribution", lw=5)
+
+    axes[1].hist(energies, bins="fd", density=True, label="Simulated distribution",
+                 color="tab:purple", alpha=0.8)
+
+    axes[1].set_xlabel(r"Energy, $E \, [\rm erg]$")
+    axes[1].set_ylabel(r"$\mathrm{d}N/\mathrm{d}E$")
+    axes[1].legend()
 
     plt.show()
 
@@ -61,7 +76,7 @@ def part_c(steps=10000):
     # create a new class for the simulation with some randomish variable choices
     N = 300
     masses = np.concatenate([np.repeat(1, N // 2), np.repeat(10, N - N // 2)])
-    sim = Simulation(N=N, E=1, size=750, radius=3, masses=masses, delay=1, visualise=False)
+    sim = Simulation(N=N, E=1, size=750, radius=3, masses=masses, delay=1, visualise=True)
 
     # start a timer and create an empty velocity array
     start = time()
@@ -80,7 +95,7 @@ def part_c(steps=10000):
     print("Runtime: {:1.2f}s".format(time() - start))
 
     # save the velocities for later (just in case)
-    np.save("data/vels_2c.npy", velocities)
+    # np.save("data/vels_2c.npy", velocities)
 
     # -- PLOTTING --
     # tile out the masses to the full range
@@ -90,16 +105,18 @@ def part_c(steps=10000):
     kBT_low = 0.5 * np.mean(velocities[full_masses == 1]**2)
     kBT_high = 0.5 * np.mean(velocities[full_masses == 10]**2)
 
-    # start a figure
     fig, ax = plt.subplots()
 
     # plot the analytic Maxwellian
     v_range = np.linspace(0, np.ceil(velocities.max()), 1000)
     ax.plot(v_range, 0.5 * (velocity_dist(v_range, kBT_low)
-                            + velocity_dist(v_range, kBT_high)), label="Mixture of Maxwellians")
+                            + velocity_dist(v_range, kBT_high)), label="Mixture of Maxwellians", lw=3)
+    ax.plot(v_range, 0.5 * velocity_dist(v_range, kBT_low), label="Individual Maxwellians", linestyle="dotted", color="darkblue", lw=2)
+    ax.plot(v_range, 0.5 * velocity_dist(v_range, kBT_high), linestyle="dotted", color="darkblue", lw=2)
 
     # plot a histogram of the velocities
-    ax.hist(velocities, bins="fd", density=True, label="Simulated distribution")
+    ax.hist(velocities, bins="fd", density=True, label="Simulated distribution",
+            color="tab:orange", alpha=0.8)
 
     # label axes, add a legend
     ax.set_xlabel(r"Velocity, $v \, [\rm cm \, s^{-1}]$")
@@ -114,11 +131,6 @@ def part_c(steps=10000):
 
     energies = 0.5 * full_masses * velocities**2
 
-    # plot the analytic Maxwellian
-    # v_range = np.linspace(0, np.ceil(velocities.max()), 1000)
-    # ax.plot(v_range, 0.5 * (velocity_dist(v_range, kBT_low)
-    #                         + velocity_dist(v_range, kBT_high)), label="Mixture of Maxwellians")
-
     # plot a histogram of the velocities
     ax.hist(energies, bins="fd", density=True, label="Simulated distribution")
 
@@ -132,7 +144,7 @@ def part_c(steps=10000):
 
 
 def main():
-    part_c(1000)
+    part_b(1000)
 
 
 if __name__ == "__main__":
