faa943435b6b7867ae01856417ca4685820af8a5
Tom Wagg <twagg@college.harvard.edu>
2022-01-18 16:52:26 -0800

    clean up plotting in p3

--- pset1/problem_3.py
+++ pset1/problem_3.py
@@ -1,8 +1,27 @@
 # import the code for the simulation
 from simulator import Simulation
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("TkAgg")
 import numpy as np
 from time import time
+
+plt.rc('font', family='serif')
+plt.rcParams['text.usetex'] = False
+fs = 18
+
+# update various fontsizes to match
+params = {'figure.figsize': (12, 8),
+          'legend.fontsize': fs,
+          'axes.labelsize': fs,
+          'xtick.labelsize': 0.9 * fs,
+          'ytick.labelsize': 0.9 * fs,
+          'axes.linewidth': 1.1,
+          'xtick.major.size': 7,
+          'xtick.minor.size': 4,
+          'ytick.major.size': 7,
+          'ytick.minor.size': 4}
+plt.rcParams.update(params)
 
 
 def t_relax_analytic(N, size, radius, E, mass):
@@ -12,16 +31,15 @@ def t_relax_analytic(N, size, radius, E, mass):
     return (n * sigma * v0)**(-1)
 
 
-def plot_N_comparison(size=1000, radius=20, E=0.1, mass=1):
+def plot_N_comparison(fig, ax, size=1000, radius=20, E=0.1, mass=1, repeats=10):
     start = time()
-
-    fig, ax = plt.subplots()
 
     N_range = np.array([25, 50, 75, 100, 125, 150, 175, 200])
 
+    t_relax_list = []
     for N in N_range:
         t_relax = []
-        for _ in range(5):
+        for _ in range(repeats):
             sim = Simulation(N=N, E=E, size=size, radius=radius, masses=mass, visualise=False)
             t_relax.append(sim.run_simulation(run_until_steadstate=True))
 
@@ -30,6 +48,9 @@ def plot_N_comparison(size=1000, radius=20, E=0.1, mass=1):
                     color="tab:orange")
         ax.scatter(N, np.median(t_relax), color="tab:orange")
         print("N =", N, "done", t_relax)
+        t_relax_list.append(t_relax)
+
+    np.save("data/t_relax_N.npy", t_relax_list)
 
     N_range_smooth = np.linspace(N_range.min(), N_range.max(), 1000)
     ax.plot(N_range_smooth, t_relax_analytic(N_range_smooth, size, radius, E, mass))
@@ -38,21 +59,18 @@ def plot_N_comparison(size=1000, radius=20, E=0.1, mass=1):
 
     print("Runtime {:1.2f}s".format(time() - start))
 
-    plt.savefig("figures/3c_compare_N.png", bbox_inches="tight")
-
-    plt.show()
+    return fig, ax
 
 
-def plot_r_comparison(N=100, size=1000, E=0.1, mass=1):
+def plot_r_comparison(fig, ax, N=100, size=1000, E=0.1, mass=1, repeats=10):
     start = time()
 
-    fig, ax = plt.subplots()
+    r_range = np.array([10, 12.5, 15, 17.5, 20, 22.5, 25, 27.5, 30])
 
-    r_range = np.array([10, 12.5, 15, 17.5, 20])
-
+    t_relax_list = []
     for radius in r_range:
         t_relax = []
-        for _ in range(5):
+        for _ in range(repeats):
             sim = Simulation(N=N, E=E, size=size, radius=radius, masses=mass, visualise=False)
             t_relax.append(sim.run_simulation(run_until_steadstate=True))
 
@@ -61,6 +79,9 @@ def plot_r_comparison(N=100, size=1000, E=0.1, mass=1):
                     color="tab:orange")
         ax.scatter(radius, np.median(t_relax), color="tab:orange")
         print("r =", radius, "done", t_relax)
+        t_relax_list.append(t_relax)
+
+    np.save("data/t_relax_r.npy", t_relax_list)
 
     r_range_smooth = np.linspace(r_range.min(), r_range.max(), 1000)
     ax.plot(r_range_smooth, t_relax_analytic(N=N, size=size, radius=r_range_smooth, E=E, mass=mass))
@@ -69,14 +90,18 @@ def plot_r_comparison(N=100, size=1000, E=0.1, mass=1):
 
     print("Runtime {:1.2f}s".format(time() - start))
 
-    plt.savefig("figures/3c_compare_radius.png", bbox_inches="tight")
-
-    plt.show()
+    return fig, ax
 
 
 def main():
-    # plot_N_comparison()
-    plot_r_comparison()
+
+    fig, axes = plt.subplots(1, 2, figsize=(18, 7))
+
+    fig, axes[0] = plot_N_comparison(fig=fig, ax=axes[0])
+    fig, axes[1] = plot_r_comparison(fig=fig, ax=axes[1])
+
+    plt.savefig("figures/3c.pdf", format="pdf", bbox_inches="tight")
+    plt.show()
 
 
 if __name__ == "__main__":
