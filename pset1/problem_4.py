dce23d6f5675edf0b6606f31150168010c31200b
Tom Wagg <twagg@college.harvard.edu>
2022-01-19 15:12:43 -0800

    simpler way of doing the same thing

--- pset1/problem_4.py
+++ pset1/problem_4.py
@@ -25,53 +25,26 @@ plt.rcParams.update(params)
 
 
 def main():
-    sim = Simulation(N=50, E=0.1, size=1000, radius=20, masses=1, visualise=False)
+    mass = 1
+    sim = Simulation(N=100, E=1, size=1000, radius=20, masses=mass, visualise=False)
     sim.run_simulation(run_until_steadystate=True)
-    sim.run_simulation(seconds=1000)
 
-    total_pressure = 0
-    for i in range(sim.N):
-        if len(sim.wall_momenta[i]) > 1 and len(sim.wall_times[i]) > 1:
-            average_momentum = np.mean(sim.wall_momenta[i][1:])
-            times = np.array(sim.wall_times[i][1:]) - np.array(sim.wall_times[i][:-1])
-            average_time = np.mean(times)
-            pressure = average_momentum / sim.size
-            total_pressure += pressure
+    sim.wall_momenta = []
 
-        if len(sim.wall_momenta_up[i]) > 1 and len(sim.wall_times_up[i]) > 1:
-            average_momentum = np.mean(sim.wall_momenta_up[i][1:])
-            times = np.array(sim.wall_times_up[i][1:]) - np.array(sim.wall_times_up[i][:-1])
-            average_time = np.mean(times)
-            pressure = average_momentum / sim.size
-            total_pressure += pressure
+    seconds = 1000
+    sim.run_simulation(seconds=seconds)
 
-        if len(sim.wall_momenta_down[i]) > 1 and len(sim.wall_times_down[i]) > 1:
-            average_momentum = np.mean(sim.wall_momenta_down[i][1:])
-            times = np.array(sim.wall_times_down[i][1:]) - np.array(sim.wall_times_down[i][:-1])
-            average_time = np.mean(times)
-            pressure = average_momentum / sim.size
-            total_pressure += pressure
-
-        if len(sim.wall_momenta_left[i]) > 1 and len(sim.wall_times_left[i]) > 1:
-            average_momentum = np.mean(sim.wall_momenta_left[i][1:])
-            times = np.array(sim.wall_times_left[i][1:]) - np.array(sim.wall_times_left[i][:-1])
-            average_time = np.mean(times)
-            pressure = average_momentum / sim.size
-            total_pressure += pressure
-
-    # sim.pressure = []
-
-    # sim.run_simulation(seconds=10000)
+    total_pressure = np.array(sim.wall_momenta).sum() / sim.size / seconds
 
     speeds = np.sqrt(np.sum(sim.vel**2, axis=1))
     v_rms = np.sqrt(np.mean(speeds))
 
     n = sim.N / sim.size**2
-    kBT = 0.5 * sim.masses[0] * v_rms**2
+    kBT = 0.5 * mass * v_rms**2
 
     print(n * kBT)
-    print(total_pressure / 1000)
-    print(total_pressure / (1000 * n * kBT))
+    print(total_pressure)
+    print(total_pressure / (n * kBT))
 
 
 if __name__ == "__main__":
--- pset1/simulator.py
+++ pset1/simulator.py
@@ -82,14 +82,7 @@ class Simulation():
         self.pos = np.array(positions)
         self.radius = radius
         self.pressure = []
-        self.wall_momenta = [[] for _ in range(self.N)]
-        self.wall_times = [[0] for _ in range(self.N)]
-        self.wall_momenta_up = [[] for _ in range(self.N)]
-        self.wall_times_up = [[0] for _ in range(self.N)]
-        self.wall_momenta_down = [[] for _ in range(self.N)]
-        self.wall_times_down = [[0] for _ in range(self.N)]
-        self.wall_momenta_left = [[] for _ in range(self.N)]
-        self.wall_times_left = [[0] for _ in range(self.N)]
+        self.wall_momenta = []
         self.time = 0
 
         # save the masses as an array, whatever is inputted
@@ -247,19 +240,8 @@ class Simulation():
         self.pos[:, 1][bottom_wall] = self.radius
         self.pos[:, 1][top_wall] = self.size - self.radius
 
-        for i in range(self.N):
-            if self.pos[i, 0] + self.radius >= self.size:
-                self.wall_momenta[i].append(2 * self.masses[i] * np.abs(self.vel[i, 0]))
-                self.wall_times[i].append(self.time)
-            elif self.pos[i, 1] + self.radius >= self.size:
-                self.wall_momenta_up[i].append(2 * self.masses[i] * np.abs(self.vel[i, 1]))
-                self.wall_times_up[i].append(self.time)
-            elif self.pos[i, 0] - self.radius <= 0:
-                self.wall_momenta_left[i].append(2 * self.masses[i] * np.abs(self.vel[i, 0]))
-                self.wall_times_left[i].append(self.time)
-            elif self.pos[i, 1] - self.radius <= 0:
-                self.wall_momenta_down[i].append(2 * self.masses[i] * np.abs(self.vel[i, 1]))
-                self.wall_times_down[i].append(self.time)
+        self.wall_momenta.extend(2 * self.masses[outside_x] * np.abs(self.vel[:, 0][outside_x]))
+        self.wall_momenta.extend(2 * self.masses[outside_y] * np.abs(self.vel[:, 1][outside_y]))
 
     def run_simulation(self, seconds=1000, run_until_steadystate=False):
         """Run the simulation of particles! It can either be run for a set amount of time or until a steady
