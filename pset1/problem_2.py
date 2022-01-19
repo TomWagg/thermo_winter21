6813ba7b05b53f62dec037a57ddcebba2770f487
Tom Wagg <twagg@college.harvard.edu>
2022-01-18 16:18:30 -0800

    switch to pdfs, update 2b plotting

--- pset1/problem_2.py
+++ pset1/problem_2.py
@@ -1,8 +1,27 @@
 # import the code for the simulation
 from simulator import Simulation
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("TkAgg")
 import numpy as np
 from time import time
+
+plt.rc('font', family='serif')
+plt.rcParams['text.usetex'] = False
+fs = 18
+
+# update various fontsizes to match
+params = {'figure.figsize': (12, 8),
+          'legend.fontsize': fs,
+          'axes.labelsize': fs,
+          'xtick.labelsize': 0.9 * fs,
+          'ytick.labelsize': 0.9 * fs,
+          'axes.linewidth': 1.1,
+          'xtick.major.size': 7,
+          'xtick.minor.size': 4,
+          'ytick.major.size': 7,
+          'ytick.minor.size': 4}
+plt.rcParams.update(params)
 
 
 def velocity_dist(v, v_rms):
@@ -43,7 +62,7 @@ def part_b(steps=10000):
     v_rms = np.sqrt(np.mean(velocities**2))
 
     # start a figure
-    fig, axes = plt.subplots(1, 2, figsize=(16, 7))
+    fig, axes = plt.subplots(1, 2, figsize=(18, 7))
 
     # plot the analytic velocity distribution
     v_range = np.linspace(0, np.ceil(velocities.max()), 1000)
@@ -70,7 +89,7 @@ def part_b(steps=10000):
     axes[1].set_ylabel(r"$\mathrm{d}N/\mathrm{d}E$")
     axes[1].legend()
 
-    plt.savefig("figures/2b.png", bbox_inches="tight")
+    plt.savefig("figures/2b.pdf", format="pdf", bbox_inches="tight")
 
     plt.show()
 
