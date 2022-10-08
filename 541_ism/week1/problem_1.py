097017af6197df1dda45ce9120f1697e7698615a
Tom Wagg <tomjwagg@gmail.com>
2022-10-08 17:51:29 -0400

    541 - finish week 1 problems

--- 541_ism/week1/problem_1.py
+++ 541_ism/week1/problem_1.py
@@ -14,6 +14,17 @@ n_H = (disc_density / (1.4 * const.m_p)).to(u.cm**(-3))
 print(f"n_H {n_H:1.1e}")
 print()
 
+v_dg = 4/3 * np.pi * (1000 * u.Angstrom)**3
+rho_dg = 2 * u.g / u.cm**3
+m_dg = (v_dg * rho_dg).to(u.g)
+print(f"Mass of a dust grain: {m_dg:1.1e}")
+
+m_dust = 0.7 / 100 * Mdisc
+N_dg = m_dust / m_dg
+n_dg = (N_dg / Vdisc).to(u.cm**(-3))
+print(f"Number density of dust grains {n_dg: 1.1e}")
+print()
+
 
 mc_vol = 4/3 * np.pi * (15 * u.pc)**3
 h2_mass = 2 * const.m_p
@@ -27,7 +38,7 @@ print()
 
 # print(f"Fraction of volume occupied {(mc_vol / Vdisc).decompose():1.1e}")
 
-v_los = (np.pi * (15 * u.pc)**2 * (8.5 * u.kpc)).to(u.cm**3)
-print("Volume along line of sight")
+v_los = (np.pi * (15 * u.pc)**2 * (8.5 * u.kpc)).to(u.pc**3)
+print(f"Volume along line of sight: {v_los:1.1e}")
 
 print((v_los / Vdisc * mc_count).decompose())
