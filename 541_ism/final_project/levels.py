525513f699b890b8c91d1fcb360fc69dd5a62cbb
Tom Wagg <tomjwagg@gmail.com>
2022-10-31 23:01:57 -0700

    sort transitions, add label options

--- 541_ism/final_project/levels.py
+++ 541_ism/final_project/levels.py
@@ -24,13 +24,16 @@ params = {'figure.figsize': (12, 8),
 plt.rcParams.update(params)
 
 
-def plot_energy_levels(spec_terms, transitions, title=None, fig=None, ax=None, show=True):
+def plot_energy_levels(spec_terms, transitions, title=None, show_term_labels=True, show_n_labels=True,
+                       fig=None, ax=None, show=True, figsize=(6, 10)):
     x_vals = np.arange(len(transitions) + 2)
 
     if fig is None or ax is None:
-        fig, ax = plt.subplots(figsize=(6, 10))
+        fig, ax = plt.subplots(figsize=figsize)
 
     max_n = max(transitions)[0]
+
+    transitions = sorted(transitions, key=lambda x: (x[1], x[0]), reverse=False)
 
     previous_L = spec_terms[0][1]
     colour_ind = 0
@@ -43,10 +46,13 @@ def plot_energy_levels(spec_terms, transitions, title=None, fig=None, ax=None, s
             height += 2
             colour_ind += 1
         ax.plot(x_vals, np.ones_like(x_vals) * height, color=f"C{colour_ind}", lw=2)
-        ax.annotate(*terms.format_terms(term, use_latex=True), xy=(-0.1, height),
-                    ha="right", va="center", fontsize=fs)
-        ax.annotate(f"n = {n + 1}", xy=(x_vals[-1] + 0.1, height),
-                    ha="left", va="center", fontsize=0.8*fs)
+
+        if show_term_labels:
+            ax.annotate(*terms.format_terms(term, use_latex=True), xy=(-0.1, height),
+                        ha="right", va="center", fontsize=fs)
+        if show_n_labels:
+            ax.annotate(f"n = {n + 1}", xy=(x_vals[-1] + 0.1, height),
+                        ha="left", va="center", fontsize=0.8*fs)
         previous_L = L
         heights.append(height)
 
@@ -59,8 +65,9 @@ def plot_energy_levels(spec_terms, transitions, title=None, fig=None, ax=None, s
 
         wavelength = transition[2].to(u.Angstrom).value
         t_string = f"{wavelength:1.1f}" if wavelength < 1e5 else f"{wavelength:1.1e}"
-        ax.annotate(t_string + r"$\rm \AA$", xy=((x + 1) * 1.01, midpoint), rotation=90,
-                    va="center", fontsize=0.6*fs, color="grey")
+        ax.annotate(t_string + r"$\rm \AA$", xy=((x + 1) + 0.125, midpoint), rotation=90,
+                    va="center", fontsize=0.6*fs, color="grey", bbox=dict(boxstyle="round", ec="none",
+                                                                          fc="white"))
 
     ax.set_xlim(-0.5, x_vals[-1] + 0.5)
     if title is not None:
